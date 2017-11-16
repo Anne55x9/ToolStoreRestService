3d504bf59132aebce021109db6b92fc90db355c0
Anne Sofie Molsted Wanscher <anne55x9@edu.easj.dk>
2017-11-16 13:28:37 +0100

    Operette interface med CRUD kald. Klassen Service1 indeholder metoderne kommandoerne Get,post,put,delete.

---
+++ ToolStoreRestService/IToolstoreService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ToolStoreRestService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IToolstoreService" in both code and config file together.
+    [ServiceContract]
+    public interface IToolstoreService
+    {
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, UriTemplate = "tools")]
+        IList<Tool> GetTools();
+
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, UriTemplate = "tools/{id}")]
+        Tool GetTool(string id);
+
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, UriTemplate = "tools/{id}/name")]
+        string GetToolName(string id);
+
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, UriTemplate = "tools/name/{nameFragment}")]
+        IEnumerable<Tool> GetToolByName(string nameFragment);
+
+        /// <summary>
+        /// Post commando kald.
+        /// </summary>
+        /// <param name="tool"></param>
+        /// <returns></returns>
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json,
+            UriTemplate = "tools")]
+        Tool AddTool(Tool tool);
+
+        /// <summary>
+        /// Update/put kommando kald.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="tool"></param>
+        /// <returns></returns>
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json,
+            UriTemplate = "tools/{id}")]
+        Tool UpdateTool(string id, Tool tool);
+
+        /// <summary>
+        /// Delete kommando kald.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+
+        [OperationContract]
+        [WebInvoke(Method = "DELETE", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "tools/{id}")]
+
+        Tool DeleteTool(string id);
+    }
+  
+}
--- ToolStoreRestService/Service1.svc.cs
+++ ToolStoreRestService/Service1.svc.cs
@@ -8,26 +8,114 @@ using System.Text;
 
 namespace ToolStoreRestService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ToolstoreService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ToolstoreService.svc or ToolstoreService.svc.cs at the Solution Explorer and start debugging.
+    public class ToolstoreService : IToolstoreService
     {
-        public string GetData(int value)
+
+        private static readonly IList<Tool> toolsList = new List<Tool>();
+        private static int _nextid = 4;
+
+        static ToolstoreService()
         {
-            return string.Format("You entered: {0}", value);
+            Tool firstTool = new Tool
+            {
+                Id = 1,
+                Name = "SuperSqruer",
+                Type = "Skruetrækker",
+                Brand = "DeckerDig",
+                Price = 100
+                
+            };
+
+            toolsList.Add(firstTool);
+
+            Tool secondTool = new Tool
+            {
+                Id = 2,
+                Name = "SlangeLange",
+                Type = "Tilbehør",
+                Brand = "Wrecks",
+                Price = 50
+
+            };
+
+            toolsList.Add(secondTool);
+
+            Tool thirdTool = new Tool
+            {
+                Id = 3,
+                Name = "Hammerhaj",
+                Type = "Slåtøj",
+                Brand = "Håkon",
+                Price = 450
+
+            };
+
+            toolsList.Add(thirdTool);
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        public IList<Tool> GetTools()
         {
-            if (composite == null)
+            return toolsList;
+        }
+
+        public Tool GetTool(string id)
+        {
+            int idNumber = int.Parse(id);
+            return toolsList.FirstOrDefault(tool => tool.Id == idNumber);
+        }
+
+        public string GetToolName(string id)
+        {
+            Tool tool = GetTool(id);
+            if (tool == null)
             {
-                throw new ArgumentNullException("composite");
+                return null;
             }
-            if (composite.BoolValue)
+            return tool.Name;
+        }
+
+        public IEnumerable<Tool> GetToolByName(string nameFragment)
+        {
+            return toolsList.Where(tool => tool.Name.ToLower().Contains(nameFragment.ToLower()));
+        }
+
+        public Tool AddTool(Tool tool)
+        {
+            tool.Id = _nextid++;
+            toolsList.Add(tool);
+            return tool;
+        }
+
+        public Tool UpdateTool(string id, Tool tool)
+        {
+            int idNumber = int.Parse(id);
+            Tool existingTool = toolsList.FirstOrDefault(b => b.Id == idNumber);
+            if (existingTool == null)
             {
-                composite.StringValue += "Suffix";
+                return null;
             }
-            return composite;
+            existingTool.Name = tool.Name;
+            existingTool.Type = tool.Type;
+            existingTool.Brand = tool.Brand;
+            existingTool.Price = tool.Price;
+            return existingTool;
+        }
+
+        public Tool DeleteTool(string id)
+        {
+            Tool tool = GetTool(id);
+            if (tool == null)
+            {
+                return null;
+            }
+            bool removed = toolsList.Remove(tool);
+            if (removed)
+            {
+                return tool;
+            }
+            return null;
         }
     }
 }
